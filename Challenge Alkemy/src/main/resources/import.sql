2ae3e7f2de79549a9a93265f46b595af67f34056
Gonzalo Godoy Espinosa <66580567+Gongodes@users.noreply.github.com>
2021-04-04 14:06:53 -0400

    Update import.sql

--- Challenge Alkemy/src/main/resources/import.sql
+++ Challenge Alkemy/src/main/resources/import.sql
@@ -1,2 +1,2 @@
 INSERT INTO students (password,name) VALUES( '1221', 'gonzalo');
-INSERT INTO students ( password,name) VALUES( '1234', 'pablo');
+INSERT INTO students ( password,name) VALUES( '1236', 'pablo');
